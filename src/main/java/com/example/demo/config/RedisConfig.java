ea8164461b2bfd197db368a756f3e25be244b5be
Thoeurn Nethan <nethanthoeurn19@gmail.com>
2023-03-07 09:36:12 +0700

    Add Jackson2JsonRedisSerializer to RedisConfig

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -6,6 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 
 @Configuration
 public class RedisConfig {
@@ -15,16 +23,47 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
     
+    
+    // Set connection
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory(){
-        RedisStandaloneConfiguration configuration= new RedisStandaloneConfiguration(host,port);
+        RedisStandaloneConfiguration configuration= new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
 		return new JedisConnectionFactory(configuration);
 	}
 
+    /* This is the default configuration and it will use java Serializer 
+    (You model class must implement Serializable) */
 	@Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String,Object> redisTemplate= new RedisTemplate<>();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
         return redisTemplate;
     }
+
+    /* This is configuration for Redis to store values as JSON. We use Jackson2JsonRedisSerializer
+    (Model class don't need to implement Serializable , You can remove it ) */
+   /* @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+
+        redisTemplate.setKeySerializer(stringRedisSerializer); // The serialization type of the key
+
+        Jackson2JsonRedisSerializer<?> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance ,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+       
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer); // The serialization type of value
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    } */
 }
